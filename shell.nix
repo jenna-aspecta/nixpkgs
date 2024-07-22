871c316b0eabdfaf69cbd416821b6bbad33e6ff5
jenna-aspecta <163962700+jenna-aspecta@users.noreply.github.com>
2024-07-22 15:16:46 +0800

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,6 +4,12 @@
 # because every time you change any file and do another `nix develop`,
 # it would create another copy of the entire ~500MB tree in the store.
 # See https://github.com/NixOS/nix/pull/6530 for the future
+# A shell to get tooling for Nixpkgs development
+#
+# Note: We intentionally don't use Flakes here,
+# because every time you change any file and do another `nix develop`,
+# it would create another copy of the entire ~500MB tree in the store.
+# See https://github.com/NixOS/nix/pull/6530 for the future
 {
   system ? builtins.currentSystem,
 }:
